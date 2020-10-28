9e08c6b30baafb314ca8e85e803fb1325b55c114
Myroslawa Chrin <fp6266@wayne.edu>
2020-10-28 16:06:53 -0400

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-//Author: Myroslawa Chrin <fp6266@wayne.edu>
+//Author: Myroslawa Chrin  <fp6266.edu>
   
 // Header file for input output functions 
 #include<iostream>  
